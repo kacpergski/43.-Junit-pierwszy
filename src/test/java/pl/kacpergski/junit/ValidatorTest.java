e061b2a13a261eaee84f6d305ccddaa2b8b4d943
Kacper Grzybkowski <kacperg@gmail.com>
2021-03-30 11:45:35 +0200

    dodano testy pesel długosc i pesel poprawnosc dnia

---
+++ src/test/java/pl/kacpergski/junit/ValidatorTest.java
@@ -0,0 +1,63 @@
+package pl.kacpergski.junit;
+
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+public class ValidatorTest {
+
+    @Test
+    public void isNotValidIfLenghth4(){
+        //given
+        String pesel = "1511";
+
+        //when
+
+        boolean isValid = Validator.peselValidation(pesel);
+
+        //then
+        assertFalse(isValid);
+
+    }
+
+    @Test
+    public void isValidIfLenghth11(){
+        //given
+        String pesel = "81011012345";
+
+        //when
+
+        boolean isValid = Validator.peselValidation(pesel);
+
+        //then
+        assertTrue(isValid);
+
+    }
+
+    @Test
+    public void isValidDayFrom1To31(){
+        //given
+        String pesel = "81011112345";
+
+        //when
+
+        boolean isValid = Validator.peselValidation(pesel);
+
+        //then
+        assertTrue(isValid);
+
+    }
+
+    @Test
+    public void isnotValidDayFrom1To31(){
+        //given
+        String pesel = "81014412345";
+
+        //when
+
+        boolean isValid = Validator.peselValidation(pesel);
+
+        //then
+        assertFalse(isValid);
+
+    }
+}
